93b4f0c138b89fd8368195213e423fff0501ac35
Sean Blankenburgh <6010924@mborijnland.nl>
2021-06-17 14:30:13 +0200

    machtengedeele van de eenvoudige rekenpagina gemaakt
    ik moet alleen nog wel zorgen dat het max veld weggaat als de tafel optie niet geselecteerd is

--- JavaScript/eenvoudige rekenpagina.js
+++ JavaScript/eenvoudige rekenpagina.js
@@ -1,19 +1,41 @@
-var tafelgetal;
+var inputgetal;
 var max;
 var teller = 1;
 var button;
 
 window.onload=function(){
 button = document.getElementById('submit');
-    button.onclick=function berekenTafel(){
+
+
+
+    button.onclick=function bereken(){
+        var som = document.getElementById("soort_som");
+        var somsoort = som.options[som.selectedIndex].value;
         document.getElementById('output').innerHTML = '';
-                    tafelgetal = document.getElementById('tafelgetal').value;
-                    max = document.getElementById('max').value;
-                    teller = 1;
-                    while(teller <= max){
-                        document.getElementById('output').innerHTML += teller + '*' + tafelgetal + '=' + teller*tafelgetal + '<br>';
-                        teller++;
-                    }
+        switch (somsoort){
+                case "tafels":
+            
+                        inputgetal = document.getElementById('inputgetal').value;
+                        max = document.getElementById('max').value;
+                        teller = 1;
+                        while(teller <= max){
+                            document.getElementById('output').innerHTML +=  teller + '*' + inputgetal + '=' + teller*inputgetal + '<br>';
+                            teller++;
+                        }
+                    break;
+
+                case "machten":
+
+                        inputgetal = document.getElementById('inputgetal').value;
+                        teller = 1;
+                        while(teller <= 15){
+                            document.getElementById('output').innerHTML += '<p>' + inputgetal + '<sup>' + teller + '</sup> =' + Math.pow(inputgetal, teller); + '</p><br>';
+                            teller++;
+                        }
+                    break;
+
+
+                }
     }
     
 }
